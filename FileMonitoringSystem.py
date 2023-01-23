9d562f88336bd74495e671cb7ccade39e2a0111d
a129924 <a129924@gmail.com>
2023-01-23 17:14:27 +0800

    update files

--- FileMonitoringSystem.py
+++ FileMonitoringSystem.py
@@ -13,15 +13,38 @@ class FileEventHandler(FileSystemEventHandler):
         self.dirs = dirs
 
         self.src_files:dict = {}
+        self.move_files:dict = {}
+
+    def now_file_size(self,path:str)->None:
+        if self.src_files[path] < os.path.getsize(path):
+            self.src_files[path] = os.path.getsize(path)
+    
+    def create_new_file(self,path:str)->None:
+        self.src_files[path] = os.path.getsize(path)
+
+    def delete_src_file(self,path:str)->None:
+        del self.src_files[path]
+
+    def delete_src_files(self,path:str, files:list)->None:
+        for file in files:
+            self.delete_src_file(os.path.join(path, file))
 
     def on_moved(self, event):
+        # TODO: 先從這邊下手
         if event.is_directory:
-            print("directory 'moved' from {0} to {1}".format(
-                event.src_path, event.dest_path))
+            print("directory 'moved' from {0} to {1}".format(event.src_path, event.dest_path))
         else:
-            print("file 'moved' from {0} to {1}".format(
-                event.src_path, event.dest_path))
+            print("file 'moved' from {0} to {1}".format(event.src_path, event.dest_path))
+
+            self.delete_src_file(event.src_path)
+
+            self.create_new_file(event.dest_path)
+            self.now_file_size(event.dest_path)
+            
         print(f"file moved size: {os.path.getsize(event.dest_path)}") # TODO: 把檔案大小塞進self.src_files[event.src_path] = os.path.getsize(event.src_path)
+        
+
+
         print("#############################")
 
     def on_created(self, event):
@@ -29,14 +52,23 @@ class FileEventHandler(FileSystemEventHandler):
             print("directory 'created':{0}".format(event.src_path))
         else:
             print("file created:{0}".format(event.src_path))
-        print(f"file created size: {os.path.getsize(event.src_path)}")
+            self.create_new_file(event.src_path)
+
+            print(self.src_files)
+
         print("#############################")
+        # print(f"file created size: {os.path.getsize(event.src_path)}") # TODO: 把檔案大小塞進self.src_files[event.src_path] = os.path.getsize(event.src_path)
 
     def on_deleted(self, event):
         if event.is_directory:
             print("directory 'deleted':{0}".format(event.src_path))
+            # files:list[str] = os.listdir(event.src_path)
+            # self.delete_src_files(event.src_path, files)
         else:
             print("file 'deleted':{0}".format(event.src_path))
+            self.delete_src_file(event.src_path)
+            
+        print(self.src_files)
         print("#############################")
 
     def on_modified(self, event):
@@ -44,7 +76,10 @@ class FileEventHandler(FileSystemEventHandler):
             print("directory 'modified':{0}".format(event.src_path))
         else:
             print("file 'modified':{0}".format(event.src_path))
-        print(f"file modified size: {os.path.getsize(event.src_path)}") # TODO: 把檔案大小塞進self.src_files[event.src_path] = os.path.getsize(event.src_path)
+
+            self.now_file_size(event.src_path)
+            print(self.src_files)
+            
         print("#############################")
 
     def run_job(self,):
--- watchdog.md
+++ watchdog.md
@@ -7,6 +7,7 @@ Date: 2023/01/22
 預設 監控資料夾:list = [r"D:\TEST\UserProfile", r"D:\TEST\ProjectFolder"]
 如果檔案從D:\TEST 這邊 會觸發 on_modified() 要注意 
 ## on_moved(self, event)
+從沒有被監控的資料夾中`複製貼上` `剪下貼上`檔案會觸發
 
 
 ## on_created(self, event)
@@ -18,4 +19,15 @@ Date: 2023/01/22
 
 
 ## on_modified(self, event)
+如果檔案從未被監控的資料夾中`複製貼上` 就會持續觸發 到移動完畢為止
 
+# 觸發順序
+
+## 複製貼上
+on_created -> on_modified 
+
+## 剪下貼上
+on_moved -> on_modified
+
+## 刪除
+on_deleted -> on_modified
