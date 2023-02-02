537b6d93c6db1bc3ddf9550c7f06fcbd959dcce2
a129924 <48639105+a129924@users.noreply.github.com>
2023-02-02 17:47:17 +0800

    add filrs

--- regex_converter.py
+++ regex_converter.py
@@ -1,4 +1,5 @@
 import re
+import os 
 from datetime import datetime
 
 import time
@@ -21,7 +22,7 @@ class CheckCommaList(list):
         else:
             return ComplexString(string)
 
-class ComplexString(str):
+class ComplexString():
     def __init__(self, string:str):
         self.string = string
         
@@ -79,4 +80,5 @@ if __name__ == "__main__":
     new_strings: list = CheckCommaList(contension_strings)
     print((now() - start)*1000)
     print(new_strings)
+    print(list(map(os.path.abspath, new_strings)))
 
