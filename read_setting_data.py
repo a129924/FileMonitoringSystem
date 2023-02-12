ddeead82357c3fef885393c10b587124e5e3277f
Andrew <a129924@gmail.com>
2023-02-13 02:04:19 +0800

    add files

---
+++ read_setting_data.py
@@ -0,0 +1,11 @@
+from ExcelDataParser import ExcelDataParser
+
+setting_data = ExcelDataParser(
+    r"D:\code\python\FileMonitoringSystem\Setting\ProjectSettingInfo新架構.xlsx",
+    "檔案格式", is_convert = False)
+
+data_ = setting_data.fillna(0, "first")
+
+data_groupby = setting_data.groupby(0, data_)
+
+print(data_groupby)
